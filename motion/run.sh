5e4b4330af56e4c894570974ee34a9520c4454b0
Der Flo <derflo.dev@gmail.com>
2018-03-07 13:16:46 +0100

    Update run.sh

--- motion/run.sh
+++ motion/run.sh
@@ -73,10 +73,10 @@ echo "Cron execution permission"
 # Give execution rights on the cron job
 chmod 0644 /etc/cron.d/motion-cron
 # Create the log file to be able to run tail
-touch /share/motion/cron.log
+#touch /share/motion/cron.log
 echo "Run cron"
 # Run the command on container startup
-cron && tail -f /share/motion/cron.log
+crontab #&& tail -f /share/motion/cron.log
 
 
 echo "[Info] Show connected usb devices"
