71dd19d8d2422d8447beb2403f7a3d60cf15a4a9
Der Flo <derflo.dev@gmail.com>
2018-03-27 13:38:19 +0200

    Update run.sh

--- motion/run.sh
+++ motion/run.sh
@@ -91,8 +91,8 @@ cp /share/motion/delete_images.sh /delete_images.sh
 
 chmod a+x /share/motion/delete_images.sh
 
-#echo "[Info] Run delete_images"
-#/share/motion/delete_images.sh &
+echo "[Info] Run delete_images"
+/share/motion/delete_images.sh &
 
 echo "[Info] Show connected usb devices"
 ls -al /dev/video*
